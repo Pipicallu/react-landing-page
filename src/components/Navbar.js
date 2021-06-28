a221b4dcf8dc0ecfc76fcc6a7701c316391f0d16
unknown <thomas.vassallo@gmail.com>
2021-06-28 13:42:39 +0100

    added button componenent

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,10 @@ import {Link} from 'react-router-dom';
 import marsLogo from '../mars.svg';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     return (
     <>
         <nav className="navbar">
@@ -10,6 +14,31 @@ function Navbar() {
                 <Link to="/" className="navbar-logo">
                    <img src={marsLogo} alt="logo" width="40" height="40"/> Red Planet Travel
                 </Link>
+                <div className="menu-icon" onClick={handleClick}>
+                    <i className={click ? 'fas fa-times': 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/packages' className='nav-links' onClick={closeMobileMenu}>
+                            Packages
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/faq' className='nav-links' onClick={closeMobileMenu}>
+                            F.A.Q
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-link-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
         </> 
